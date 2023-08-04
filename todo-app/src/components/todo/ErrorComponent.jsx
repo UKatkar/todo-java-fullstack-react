3313093260f17911f57e91b7648c6793ceda88a4
umesh.katkar <uvkatkar@gmail.com>
2023-08-05 01:18:27 +0530

    Separating components to ease of manage code.

---
+++ todo-app/src/components/todo/ErrorComponent.jsx
@@ -0,0 +1,12 @@
+function ErrorComponent () {
+    return (
+        <div className="ErrorComponent">
+            <h1>We are working realy hard!</h1>
+            <div>
+                Apologies for 404. Reach out our team at ABC-DEF-GHIj
+            </div>
+        </div>
+    )
+}
+
+export default ErrorComponent
---
+++ todo-app/src/components/todo/LoginComponent.jsx
@@ -0,0 +1,58 @@
+import { useNavigate} from 'react-router-dom'
+import { useState } from 'react'
+
+export default function LoginComponent () {
+
+    const[username, setUsername] = useState('Umesh')
+
+    const[password, setPassword] = useState('dummy')
+
+    const[showSuccessMessage, setShowSuccessMessage] = useState(false)
+
+    const[showErrorMessage, setShowErrorMessage] = useState(false)
+
+    const navigate = useNavigate()
+
+    function handleUsernameChange (event) { 
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange (event) {
+        setPassword(event.target.value)
+    }
+
+    function handleSubmit (){
+        if(username === 'Umesh' && password === 'dummy'){
+            setShowSuccessMessage(true)
+            setShowErrorMessage(false)
+            navigate(`/welcome/${username}`)
+        } else {
+            setShowSuccessMessage(false)
+            setShowErrorMessage(true)
+        }
+    }
+
+    return (
+        <div className="Login">
+            <h1>Time to login</h1>
+            <div>
+                {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
+                {showErrorMessage && <div className="errorMessage">Authenticated Failed. 
+                                                                    Please check your credentials.</div>}
+                <div className="LoginForm">
+                    <div>
+                        <label>User Name</label>
+                        <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
+                    </div>
+                    <div>
+                        <button type="button" name="login" onClick={handleSubmit}>Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
